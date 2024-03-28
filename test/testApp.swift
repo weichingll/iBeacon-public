02660d93e8ce83ebf3c92f89d720c6587757724b
weichingll <113404422+weichingll@users.noreply.github.com>
2024-03-28 22:34:22 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 林京緯 on 2024/3/28.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
