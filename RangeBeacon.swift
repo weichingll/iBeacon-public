1dcf90c460b1ded08b17552d7e245ab2be002373
weichingll <113404422+weichingll@users.noreply.github.com>
2024-04-15 11:58:14 +0800

    登入與註冊基本頁面

---
+++ RangeBeacon.swift
@@ -0,0 +1,18 @@
+//
+//  RangeBeacon.swift
+//  test
+//
+//  Created by 林京緯 on 2024/4/14.
+//
+
+import SwiftUI
+
+struct RangeBeacon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RangeBeacon()
+}
